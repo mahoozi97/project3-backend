0b9e2ecc9f832cd143a5a527efb7be5c960cbe4c
mahoozi97 <mahoozi97@icloud.com>
2026-04-14 20:27:18 +0300

    I added the booking routes for create and get all

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const cors = require('cors');
-const logger = require('morgan');
-const authRouter = require('./controllers/auth.routes');
-const verifyToken = require('./middleware/verify-token');
-
+const mongoose = require("mongoose");
+const cors = require("cors");
+const logger = require("morgan");
+const authRouter = require("./controllers/auth.routes");
+const bookingRouter = require("./controllers/booking.routes");
+const verifyToken = require("./middleware/verify-token");
 
 mongoose.connect(process.env.MONGODB_URI);
 
-mongoose.connection.on('connected', () => {
+mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
 app.use(cors());
 app.use(express.json());
-app.use(logger('dev'));
+app.use(logger("dev"));
 
 // Routes go here
-app.use('/auth', authRouter);
-
+app.use("/auth", authRouter);
+app.use("/booking", verifyToken, bookingRouter);
 
 app.listen(3000, () => {
-  console.log('The express app is ready!');
+  console.log("The express app is ready!");
 });
