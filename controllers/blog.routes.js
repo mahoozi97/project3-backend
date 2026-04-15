18ee9f627139e146949c1fc5bb4472da10084aa9
AhmedofHazeem <ahmedalihazeem24@gmail.com>
2026-04-15 09:41:29 +0300

    Get a single blog by ID

--- controllers/blog.routes.js
+++ controllers/blog.routes.js
@@ -1,2 +1,45 @@
-const router = require("express").Router();
+const express = require("express").Router();
 const Blog = require("../models/Blog");
+
+//Create a new blog
+router.post("/", async (req, res) => {
+  try {
+    const { description, image } = req.body;
+    const newBlog = new Blog({
+      description,
+      image,
+      comments: [],
+    });
+    const savedBlog = await newBlog.save();
+    res.status(201).json(savedBlog);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+//get all the blogs
+router.get("/", async (req, res) => {
+  try {
+    const blogs = await Blog.find().populate("comments.userId", "name email");
+    res.json(blogs);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//get a single blog by ID
+router.get("/:id", async (req, res) => {
+  try {
+    const blog = await Blog.findById(req.params.id).populate(
+      "comments.userId",
+      "name email",
+    );
+    if (!blog) {
+      return res.status(404).json({ message: "Blog not found" });
+    }
+
+    res.json(blog);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
