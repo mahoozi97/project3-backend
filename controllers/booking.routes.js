5215660969274af494555eee3dca0f713a934df2
Ali Almahoozi <123273436+mahoozi97@users.noreply.github.com>
2026-04-15 16:32:09 +0300

    add booking routes (get by user, update, delete)

--- controllers/booking.routes.js
+++ controllers/booking.routes.js
@@ -9,8 +9,8 @@ router.post("/", async (req, res) => {
     console.log("✅ new booking: ", createdBooking);
     res.status(201).json(createdBooking);
   } catch (error) {
-    console.log("❌ error creat a new booking: ", error);
-    res.status(500).json(error);
+    console.log("❌ error create a new booking: ", error);
+    res.status(500).json({ error: error.message });
   }
 });
 
@@ -21,8 +21,67 @@ router.get("/", async (req, res) => {
     console.log("✅ fetched all booking successfully: ", allBooking);
     res.status(200).json(allBooking);
   } catch (error) {
-    console.log("❌ error fetch all booking: ", error);
-    res.status(500).json(error);
+    console.log("❌ Error fetching all bookings: ", error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// get by userId
+router.get("/my-booking", async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const bookingByUserId = await Booking.find({ userId }).sort({ _id: -1 });
+    console.log("✅ fetched booking by userId successfully: ", bookingByUserId);
+    res.status(200).json(bookingByUserId);
+  } catch (error) {
+    console.log("❌ Error fetching booking by userId: ", error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// edit the booking
+router.put("/:id", async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const foundBooking = await Booking.findById(req.params.id);
+
+    if (!foundBooking.userId.equals(userId)) {
+      return res
+        .status(403)
+        .json({ error: "You are not authorized to edit this booking" });
+    }
+
+    const updatedBooking = await Booking.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true },
+    );
+    console.log("✅ Booking updated successfully: ", updatedBooking);
+    res.status(200).json(updatedBooking);
+  } catch (error) {
+    console.log("❌ Error updating booking: ", error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// delete the booking
+router.delete("/:id", async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const foundBooking = await Booking.findById(req.params.id);
+
+    if (!foundBooking.userId.equals(userId)) {
+      return res
+        .status(403)
+        .json({ error: "You are not authorized to cancel this booking" });
+    }
+
+    const deletedBooking = await Booking.findByIdAndDelete(req.params.id);
+    console.log("✅ Booking deleted successfully: ", deletedBooking);
+    res.status(200).json(deletedBooking);
+  } catch (error) {
+    console.log("❌ Error deleting booking: ", error);
+    res.status(500).json({ error: error.message });
   }
 });
 
