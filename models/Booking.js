1d46168376d72e475b209b232f5c9a613ef219ca
mahoozi97 <mahoozi97@icloud.com>
2026-04-25 06:57:32 +0300

    Update booking status enum to include 'Completed' option

--- models/Booking.js
+++ models/Booking.js
@@ -29,7 +29,7 @@ const bookingSchema = new mongoose.Schema(
     },
     status: {
       type: String,
-      enum: ["Pending ⏳", "Accepted ✅", "Cancelled ❌"],
+      enum: ["Pending ⏳", "Accepted ✅", "Completed ✔️", "Cancelled ❌"],
       default: "Pending ⏳",
     },
     userId: {
